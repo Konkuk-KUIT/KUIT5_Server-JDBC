ab79bb4789dd6b09beebe5364b63a917a2b3c5ba
holdn2 <yoocy01@gmail.com>
2025-04-27 21:53:05 +0900

    미션 2단계 요구사항 3 예외처리. 자원회수로 발생하는 불필요한 코드 영역 제거

---
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -0,0 +1,50 @@
+package core.jdbc;
+
+import jwp.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JdbcTemplate {
+    public void update(String sql, PreparedStatementSetter preparedStatementSetter) throws SQLException {
+        try(Connection connection = ConnectionManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ){
+            preparedStatementSetter.setValues(preparedStatement);
+            preparedStatement.executeUpdate();
+        }
+    }
+
+    public List query(String sql, PreparedStatementSetter preparedStatementSetter, RowMapper rowMapper) throws SQLException {
+        try(Connection connection = ConnectionManager.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            ){
+            preparedStatementSetter.setValues(preparedStatement);
+
+            try(ResultSet resultSet = preparedStatement.executeQuery()){
+                List result  = new ArrayList<>();
+                while(resultSet.next()){
+                    result.add(rowMapper.mapRow(resultSet));
+                }
+
+                return result;
+            }
+        }
+    }
+
+    public Object queryForObject(String sql, PreparedStatementSetter preparedStatementSetter, RowMapper rowMapper)throws SQLException {
+        List result = query(sql,preparedStatementSetter,rowMapper);
+        if(result.size()==0){
+            return null;
+        }
+        return result.get(0);
+    }
+
+//    public abstract String createdQuery();
+//    public abstract void setValues(PreparedStatement preparedStatement) throws SQLException;
+//    public abstract Object mapRow(ResultSet resultSet) throws SQLException;
+}
