7adb01ec730b31807c5295677b2ad65d3cf78ca5
sudo-Terry <contact.terrykim@gmail.com>
2025-04-28 02:37:32 +0900

    JdbcTemplate 추상클래스 구현

---
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -0,0 +1,31 @@
+package core.jdbc;
+
+import jwp.model.User;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public abstract class JdbcTemplate {
+    public void update(User user) throws SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        String sql = createQuery();
+        try {
+            connection = ConnectionManager.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            setValues(user, preparedStatement);
+            preparedStatement.executeUpdate();
+        }finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }if (connection != null) {
+                connection.close();
+            }
+        }
+    }
+
+    public abstract String createQuery();
+
+    public abstract void setValues(User user, PreparedStatement preparedStatement) throws SQLException;
+}
--- src/main/java/jwp/dao/UserDao.java
+++ src/main/java/jwp/dao/UserDao.java
@@ -1,26 +1,114 @@
 package jwp.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import core.jdbc.ConnectionManager;
+import core.jdbc.JdbcTemplate;
 import jwp.model.User;
 
 public class UserDao {
     public void insert(User user) throws SQLException {
-        //TODO 구현 하기
+        JdbcTemplate jdbcTemplate = new JdbcTemplate() {
+            @Override
+            public String createQuery() {
+                return "INSERT INTO Users VALUES(?, ?, ?, ?)";
+            }
+
+            @Override
+            public void setValues(User user, PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, user.getUserId());
+                preparedStatement.setString(2, user.getPassword());
+                preparedStatement.setString(3, user.getName());
+                preparedStatement.setString(4, user.getEmail());
+            }
+        };
+        jdbcTemplate.update(user);
     }
 
     public User findByUserId(String userId) throws SQLException {
-        //TODO 구현 하기
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        String sql = "SELECT * FROM Users WHERE userId = ?";
+        ResultSet resultSet = null;
+        try {
+            connection = ConnectionManager.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, userId);
+            resultSet = preparedStatement.executeQuery();
+
+            User user = null;
+            if (resultSet.next()) {
+                user = new User(resultSet.getString("userId"),
+                        resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email")
+                );
+                return user;
+            }
+        }finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
         return null;
     }
 
     public void update(User user) throws SQLException {
-        //TODO 구현 하기
+        JdbcTemplate jdbcTemplate = new JdbcTemplate() {
+            @Override
+            public String createQuery() {
+                return "UPDATE Users SET password = ?, name = ?, email = ?";
+            }
+
+            @Override
+            public void setValues(User user, PreparedStatement preparedStatement) throws SQLException {
+                preparedStatement.setString(1, user.getPassword());
+                preparedStatement.setString(2, user.getName());
+                preparedStatement.setString(3, user.getEmail());
+            }
+        };
+        jdbcTemplate.update(user);
     }
 
     public List<User> findAll() throws SQLException {
-        //TODO 구현 하기
-        return new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        String sql = "SELECT * FROM Users";
+        ResultSet resultSet = null;
+
+        try {
+            connection = ConnectionManager.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            List<User> users = new ArrayList<>();
+
+            User user = null;
+            while (resultSet.next()) {
+                user = new User(resultSet.getString("userId"),
+                        resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email")
+                );
+                users.add(user);
+            }
+
+            return users;
+        }finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 }
