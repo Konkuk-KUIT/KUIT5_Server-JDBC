988d7be4c24d4548901cbab3fe6731d9c133de01
jyunee <95419701+jynnnnnn@users.noreply.github.com>
2025-05-04 09:01:31 +0900

    [Refactor] 1단계: JDBC 함수 구현

--- src/main/java/jwp/dao/UserDao.java
+++ src/main/java/jwp/dao/UserDao.java
@@ -1,26 +1,137 @@
 package jwp.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
+
+import core.jdbc.ConnectionManager;
 import jwp.model.User;
 
 public class UserDao {
     public void insert(User user) throws SQLException {
-        //TODO 구현 하기
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            String sql = "INSERT INTO Users VALUES (?, ?, ?, ?)"; // ? <- 파라미터처럼 사용 가능
+            connection = ConnectionManager.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, user.getUserId());
+            preparedStatement.setString(2, user.getPassword());
+            preparedStatement.setString(3, user.getName());
+            preparedStatement.setString(4, user.getEmail());
+
+            preparedStatement.executeUpdate();
+        } finally {
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
     public User findByUserId(String userId) throws SQLException {
-        //TODO 구현 하기
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        String sql = "SELECT * FROM Users WHERE userId = ?";
+
+        try {
+            connection = ConnectionManager.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1, userId);
+            resultSet = preparedStatement.executeQuery();
+
+            User user = null;
+            if (resultSet.next()) {
+                user = new User(resultSet.getString("userId"), resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"));
+                return user;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        }
         return null;
     }
 
     public void update(User user) throws SQLException {
-        //TODO 구현 하기
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        String sql = "UPDATE Users SET password = ? ,name = ?, email = ? WHERE userId = ?";
+
+        try {
+            connection = ConnectionManager.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+
+            preparedStatement.setString(1, user.getPassword());
+            preparedStatement.setString(2, user.getName());
+            preparedStatement.setString(3, user.getEmail());
+            preparedStatement.setString(4, user.getUserId());
+
+            preparedStatement.executeUpdate();
+
+        } finally {
+
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        }
     }
 
     public List<User> findAll() throws SQLException {
-        //TODO 구현 하기
-        return new ArrayList<>();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        List<User> users = new ArrayList<>();
+        ResultSet resultSet = null;
+
+        String sql = "SELECT * FROM Users";
+
+        try {
+            connection = ConnectionManager.getConnection();
+            preparedStatement = connection.prepareStatement(sql);
+            resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                User user = new User(resultSet.getString("userId"),
+                        resultSet.getString("password"),
+                        resultSet.getString("name"),
+                        resultSet.getString("email"));
+
+                users.add(user);
+            }
+
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+            if (preparedStatement != null) {
+                preparedStatement.close();
+            }
+
+            if (connection != null) {
+                connection.close();
+            }
+        }
+        return users;
     }
 }
