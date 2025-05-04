02be073acc2a39ab70b5bf9cfd63d02b5eee812a
김종혁 <jonnykim1o28@gmail.com>
2025-05-04 19:56:12 +0900

    요구사항 2단계

--- src/main/java/core/jdbc/JdbcTemplate.java
+++ src/main/java/core/jdbc/JdbcTemplate.java
@@ -1,26 +1,55 @@
 package core.jdbc;
 
-import jwp.model.User;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
-public abstract class JdbcTemplate {
+public class JdbcTemplate {
 
-    public void update(){
+    public void update(String sql, PreparedStatementSetter preparedStatementSetter) {
         try {
             try (Connection connection = ConnectionManager.getConnection();
-                 PreparedStatement preparedStatement = connection.prepareStatement(createQuery());) {
-                setValues(preparedStatement);
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+                preparedStatementSetter.setValues(preparedStatement);
                 preparedStatement.executeUpdate();
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException(e);
         }
 
     }
 
-    public abstract String createQuery();
-    public abstract void setValues(PreparedStatement preparedStatement);
+    public <T> List<T> query(String sql, PreparedStatementSetter preparedStatementSetter, RowMapper rowMapper) {
+        ArrayList<T> result = new ArrayList<>();
+        try {
+            try (Connection connection = ConnectionManager.getConnection();
+                 PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
+                preparedStatementSetter.setValues(preparedStatement);
+                try (ResultSet resultSet = preparedStatement.executeQuery();) {
+                    while (resultSet.next()) {
+                        result.add((T)rowMapper.mapRow(resultSet));
+                    }
+
+                }
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+
+
+    public Object queryForObject(String sql, PreparedStatementSetter preparedStatementSetter, RowMapper rowMapper) {
+        List result = query(sql, preparedStatementSetter, rowMapper);
+        if (result.size() == 0)
+            return null;
+
+        return result.getFirst();
+    }
+
+
 }
--- src/main/java/jwp/dao/UserDao.java
+++ src/main/java/jwp/dao/UserDao.java
@@ -1,24 +1,22 @@
 package jwp.dao;
 
 import java.sql.*;
-import java.util.ArrayList;
 import java.util.List;
 
-import core.jdbc.ConnectionManager;
 import core.jdbc.JdbcTemplate;
-import core.jdbc.SelectJdbcTemplate;
+import core.jdbc.PreparedStatementSetter;
+import core.jdbc.RowMapper;
 import jwp.model.User;
 
 public class UserDao {
     public void insert(User user) throws SQLException {
-        JdbcTemplate jdbcTemplate = new JdbcTemplate() {
-            @Override
-            public String createQuery() {
-                return "INSERT INTO Users VALUES (?, ?, ?, ?)";
-            }
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
+        String sql = "INSERT INTO Users VALUES (?, ?, ?, ?)";
+
+        PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter() {
             @Override
-            public void setValues(PreparedStatement preparedStatement) {
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
                 try {
                     preparedStatement.setString(1, user.getUserId());
                     preparedStatement.setString(2, user.getPassword());
@@ -29,56 +27,56 @@ public class UserDao {
                 }
             }
         };
-        jdbcTemplate.update();
+
+        jdbcTemplate.update(sql, preparedStatementSetter);
     }
 
 
     public User findByUserId(String userId) throws SQLException {
 
-        SelectJdbcTemplate jdbcTemplate = new SelectJdbcTemplate() {
-            @Override
-            public String createQuery() {
-                return "SELECT * FROM Users WHERE userId = ?";
-            }
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
+        String sql = "SELECT * FROM Users WHERE userId = ?";
+
+        PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter() {
             @Override
-            public void setValues(PreparedStatement preparedStatement) {
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
                 try {
                     preparedStatement.setString(1, userId);
                 } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
-
             }
+        };
 
+        RowMapper rowMapper = new RowMapper() {
             @Override
-            public Object mapRow(ResultSet resultSet) {
+            public Object mapRow(ResultSet resultSet) throws SQLException {
                 try {
                     return new User(resultSet.getString("userId"),
                             resultSet.getString("password"),
                             resultSet.getString("name"),
                             resultSet.getString("email"));
 
-                }catch (SQLException e) {
+                } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
             }
         };
 
-        return jdbcTemplate.readOne();
+
+        return (User) jdbcTemplate.queryForObject(sql, preparedStatementSetter, rowMapper);
     }
 
 
     public void update(User user) throws SQLException {
 
-        JdbcTemplate jdbcTemplate = new JdbcTemplate() {
-            @Override
-            public String createQuery() {
-                return "UPDATE Users SET password = ?, name = ?, email = ?";
-            }
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
+        String sql = "UPDATE Users SET password = ?, name = ?, email = ?";
 
+        PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter() {
             @Override
-            public void setValues(PreparedStatement preparedStatement) {
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
                 try {
                     preparedStatement.setString(1, user.getPassword());
                     preparedStatement.setString(2, user.getName());
@@ -88,37 +86,41 @@ public class UserDao {
                 }
             }
         };
-        jdbcTemplate.update();
+
+
+        jdbcTemplate.update(sql, preparedStatementSetter);
     }
 
 
     public List<User> findAll() throws SQLException {
 
-        SelectJdbcTemplate jdbcTemplate = new SelectJdbcTemplate() {
-            @Override
-            public String createQuery() {
-                return "SELECT * FROM Users";
-            }
+        JdbcTemplate jdbcTemplate = new JdbcTemplate();
 
-            @Override
-            public void setValues(PreparedStatement preparedStatement) {
-            }
 
+        String sql = "SELECT * FROM Users";
+
+        PreparedStatementSetter preparedStatementSetter = new PreparedStatementSetter() {
             @Override
-            public Object mapRow(ResultSet resultSet) {
+            public void setValues(PreparedStatement preparedStatement) throws SQLException {
+
+            }
+        };
+
+        RowMapper rowMapper = new RowMapper() {
+            @Override
+            public Object mapRow(ResultSet resultSet) throws SQLException {
                 try {
                     return new User(resultSet.getString("userId"),
                             resultSet.getString("password"),
                             resultSet.getString("name"),
                             resultSet.getString("email"));
 
-                }catch (SQLException e) {
+                } catch (SQLException e) {
                     throw new RuntimeException(e);
                 }
             }
-
         };
 
-        return jdbcTemplate.readAll();
+        return jdbcTemplate.query(sql, preparedStatementSetter, rowMapper);
     }
 }
