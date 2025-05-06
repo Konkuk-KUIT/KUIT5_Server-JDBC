6ea68d01c8eba082f763ba2a597fe0cbb20212ea
yeonjy73 <yeon703@naver.com>
2025-05-06 22:03:18 +0900

    chore: 안쓰는 import 문 삭제

--- src/main/java/core/mvc/DispatcherServlet.java
+++ src/main/java/core/mvc/DispatcherServlet.java
@@ -1,7 +1,6 @@
 package core.mvc;
 
 import java.io.IOException;
-import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -11,10 +10,9 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "dispatcher", urlPatterns = "/", loadOnStartup = 1)
 public class DispatcherServlet extends HttpServlet {
     private RequestMapping requestMapping;
-    private static final String REDIRECT_PREFIX = "redirect:";
 
     @Override
-    public void init() throws ServletException {
+    public void init() {
         requestMapping = new RequestMapping();
     }
 
